c8e69c8e5c6b4a1d0b5a820b9f6d4adbd56443f4
Rashid J. Almheiri <r.muhairi@pm.me>
2025-05-02 19:17:46 +0400

    i like my nix dripping

--- flake.nix
+++ flake.nix
@@ -1,39 +1,19 @@
 {
   description = ''
-    github:huwaireb/System: Flaked System Configuration for MacOS & NixOS
+    github:huwaireb/System: Flaked System Configuration for MacOS
   '';
 
   outputs =
     inputs@{ nixpkgs, darwin, ... }:
     let
-      inherit (builtins) groupBy readDir listToAttrs;
-      inherit (nixpkgs.lib) assertMsg mapAttrs attrsToList;
-
-      lib' = import ./lib {
+      lib = import ./lib {
         inherit inputs;
         lib = nixpkgs.lib.extend (_: _: darwin.lib);
       };
-
-      hosts =
-        readDir ./hosts
-        |> mapAttrs (
-          host: type:
-          assert assertMsg (type == "directory") "All files in hosts/ should be directories (flake.nix)";
-          import ./hosts/${host} lib'
-        )
-        |> attrsToList
-        |> groupBy (
-          { value, ... }:
-          let
-            class0 = fail: if value.class == "nixos" then "nixosConfigurations" else fail;
-            class1 = fail: if value.class == "darwin" then "darwinConfigurations" else fail;
-            classFail = throw "unreachable: The class of the system is unhandled (flake.nix)";
-          in
-          class0 (class1 classFail)
-        )
-        |> mapAttrs (_: listToAttrs);
     in
-    hosts // { };
+    {
+      darwinConfigurations.moon = import ./hosts/moon lib;
+    };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
