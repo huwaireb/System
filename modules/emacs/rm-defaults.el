e075cc546568be8db466962d1cdb434d5aa62374
Rashid Almheiri <pub@rmu.ae>
2025-05-24 23:04:22 +0400

    feat(emacs): explicitly handle backups
    yoinked from gavinok's emacs.d

--- modules/emacs/rm-defaults.el
+++ modules/emacs/rm-defaults.el
@@ -5,9 +5,17 @@
 (savehist-mode +1) ; Persist minibuffer and command history across session
 
 ;; Configuring file and buffer behavior
-(setopt auto-save-default nil        ; Disable auto-saving
-        create-lockfiles nil         ; Prevent creation of lockfiles to avoid clutter
-        delete-by-moving-to-trash t) ; Move deleted files to system trash
+(setopt create-lockfiles nil  ; Useful only if you have multiple emacs sessions editing same file. Otherwise clutter.
+        auto-save-default nil ; I periodically save, backups are enough.
+        delete-by-moving-to-trash t)
+
+(setopt backup-directory-alist `(("." . ,(locate-user-emacs-file "backups")))
+        vc-make-backup-files t ; Make backups even for files under version control
+        version-control t      ; Enable versioned backups
+        backup-by-copying t    ; Create backups by copying the file instead of renaming.
+        kept-old-versions 0
+        kept-new-versions 10
+        delete-old-versions t)
 
 ;; Customizing user interface
 (setopt ring-bell-function 'ignore      ; Suppress audible bell for quieter operation
@@ -15,7 +23,7 @@
         inhibit-startup-screen t        ; Skip startup screen for faster launch
         display-line-numbers 'relative) ; Show line numbers as relative
 
-;; Defining default frame appearance
+;; Defining default frame appearance (TODO: Calculate almost maximum based on resolution)
 (setopt default-frame-alist   
         '((font . "Iosevka-18")
           (top . 77)            
@@ -24,7 +32,7 @@
           (height . 37)))  
 
 ;; Setting up shell environment integration
-(when (or (daemonp) (memq window-system '(mac ns x)))
+(when (or (daemonp) (memq window-system '(mac ns x)))          ; Only load when in daemon or window-system
   (require 'exec-path-from-shell)                              ; Load shell environment sync library
   (setopt exec-path-from-shell-variables
           '("PATH" "MANPATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"
