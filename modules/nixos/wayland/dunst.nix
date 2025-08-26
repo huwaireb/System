dbc1fa64058c4d2e875fbd60b5cb19f144f47d86
Rashid Almheiri <pub@rmu.ae>
2025-08-26 18:32:03 +0400

    feat(nixos/wayland): dunst stuff

--- modules/nixos/wayland/dunst.nix
+++ modules/nixos/wayland/dunst.nix
@@ -3,6 +3,32 @@
     {
       services.dunst = {
         enable = true;
+
+        settings.global = {
+          width = "(300, 900)";
+
+          gap_size = 5;
+          corner_radius = 8;
+          padding = 10;
+        };
+
+        settings.urgency_low = {
+          background = "#16161e";
+          foreground = "#c0caf5";
+          frame_color = "#c0caf5";
+        };
+
+        settings.urgency_normal = {
+          background = "#1a1b26";
+          foreground = "#c0caf5";
+          frame_color = "#c0caf5";
+        };
+
+        settings.urgency_critical = {
+          background = "#292e42";
+          foreground = "#db4b4b";
+          frame_color = "#db4b4b";
+        };
       };
     }
   ];
